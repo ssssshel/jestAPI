804888b40f0287feb2dae629956a3474f8816ef9
ssssshel <angelarteaga989@gmail.com>
2022-04-22 16:57:04 -0500

    tested satisfactory cases for get and post

--- __test__/users.test.js
+++ __test__/users.test.js
@@ -1,11 +1,21 @@
 const request = require('supertest')
 const app = require('../api')
-const nanoid = require('nanoid')
+const {nanoid} = require('nanoid')
+
+// al emplear un servidor se tienen que configurar las pruebas antes y despues de reaizarlas
+let testServer
+beforeAll(() => {
+	testServer = app.listen(4000)
+})
+
+afterAll((done) => {
+	testServer.close(done)
+})
 
 // supertest permite hacer consultas http desde la interfaz de testing
 
 describe('GET /api/users', () => {
-	test('should return all users', () => {
+	test('should return all users', async () => {
 		const response = await request(app).get('/api/users')
 
 		// se testea que error sea false
@@ -22,5 +32,31 @@ describe('GET /api/users', () => {
 
 		// se testea la longitud del array
 		expect(response.body.body.length).toBe(2);
+	});
+});
+
+describe('GET /api/users/:id', () => {
+	test('should get an exercise', async() => {
+		const response  = await request(app).get('/api/users/1')
+		expect(response.error).toBe(false);
+		expect(response.status).toBe(200);
+		expect(response.body.body).not.toBeNull();
+		expect(response.body.body.id).toBe("1")
+	});
+});
+
+describe('POST /api/users', () => {
+	test('should POST a new user', async() => {
+		const userId = nanoid()
+		let user = {
+			id: userId,
+			name: 'Nuevo usuario',
+			username: 'nuevo_usuario'
+		}
+		const response = await request(app).post('/api/users').send(user)
+		expect(response.error).toBe(false);
+		expect(response.status).toBe(200);
+		expect(response.body.body).not.toBeNull();
+		expect(response.body.body.id).toBe(userId);
 	});
 });
